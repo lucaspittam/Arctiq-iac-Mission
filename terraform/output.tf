80c13b649369b0e72481e32f61c7c3f8c90827e7
Lucas Pittam <98059989+lucaspittam@users.noreply.github.com>
2022-10-19 10:45:55 -0400

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -6,7 +6,7 @@ resource "local_sensitive_file" "private_key" {
   file_permission   = "0600"
 }
 resource "local_file" "ansible_inventory" {
-  content = templatefile("${path.module}/ansible/inventory.tmpl", {
+  content = templatefile("${path.module}./ansible/inventory.tmpl", {
       ip          = aws_instance.web1.public_ip,
       ssh_keyfile = local_sensitive_file.private_key.filename
   })
