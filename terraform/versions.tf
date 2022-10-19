25c97e85ee4dc524173c19f6ec316621b7f52a28
Lucas Pittam <98059989+lucaspittam@users.noreply.github.com>
2022-10-19 13:20:51 -0400

    test

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,18 +1,12 @@
 
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-  cloud {
-    organization = "Training1997"
- 
-    hostname = "app.terraform.io"
 
-    workspaces {
-      tags = ["Arctiq-iac-Mission"]
+  cloud {
+      organization = "Training1997"
+
+      workspaces {
+        name = "Arctiq-iac-Mission"
+      }
     }
-  }
+  
 }
