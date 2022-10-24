07e81cd41f9d0b370dfd61bf50b17afddc4b11fa
Lucas Pittam <98059989+lucaspittam@users.noreply.github.com>
2022-10-24 09:25:08 -0400

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,15 +3,6 @@ variable "aws_region" {
   default = "us-east-2"
 }
 
-variable "AWS_ACCESS_KEY_ID" {
-  type = string
-  default = ""
-}
-variable "AWS_SECRET_ACCESS_KEY" {
-  type = string
-  default = ""
-}
-
 variable "instance_type" {
   type = string
   default = "t2.micro"
